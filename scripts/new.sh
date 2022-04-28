084c4dfcfab938cce304dc89cddb6d294ddd3cab
AntonGashi <antongashi12345@gmail.com>
2022-04-28 18:18:31 +0100

    added script

---
+++ scripts/new.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mv ../.dwm ~/Downloads
+mv ../dmenu ~/Downloads
+mv ../dwm ~/ Downloads
+mv ../scripts ~/Downloads
+mv ../slstatus ~/Downloads
+mv ../st ~/Downloads
+mv ../surf ~/Downloads
+mv ../tabbed ~/Downloads
+mv ../.zshrc  ~/Downloads
+mv ../.config/nvim ~/Downloads
+
