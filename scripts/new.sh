9b0354e9b44e23a6477245f9ff64b43304b82c27
AntonGashi <antongashi12345@gmail.com>
2022-04-28 18:30:33 +0100

    added fixed script

--- scripts/new.sh
+++ scripts/new.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-mv ../.dwm ~/Downloads
-mv ../dmenu ~/Downloads
-mv ../dwm ~/ Downloads
-mv ../scripts ~/Downloads
-mv ../slstatus ~/Downloads
-mv ../st ~/Downloads
-mv ../surf ~/Downloads
-mv ../tabbed ~/Downloads
-mv ../.zshrc  ~/Downloads
-mv ../.config/nvim ~/Downloads
-
+mv ~/custom_programs/.dwm ~/
+mv ~/custom_programs/dmenu ~/
+mv ~/custom_programs/dwm ~/ 
+mv ~/custom_programs/scripts ~/
+mv ~/custom_programs/slstatus ~/
+mv ~/custom_programs/st ~/
+mv ~/custom_programs/surf ~/
+mv ~/custom_programs/tabbed ~/
+mv ~/custom_programs/.zshrc  ~/
+mv ~/custom_programs/.config/nvim ~/.config
