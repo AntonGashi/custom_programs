6c3a535297bc0c63ffd59cce5d25448bf042f8bd
AntonGashi <58042983+AntonGashi@users.noreply.github.com>
2022-05-17 19:23:50 +0100

    Add files via upload

--- nvim/lua/user/lsp_cmp.lua
+++ nvim/lua/user/lsp_cmp.lua
@@ -35,9 +35,9 @@ require"lspconfig".remark_ls.setup{
 }
 
 vim.diagnostic.config({
-  virtual_text = false
+  virtual_text = true
 })
 
 -- Show line diagnostics automatically in hover window
-vim.o.updatetime = 250
-vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
+--vim.o.updatetime = 250
+--vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
