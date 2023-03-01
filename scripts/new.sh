8f0eee494f03e9218f0aafdc7521505b3ba20564
AntonGashi <58042983+AntonGashi@users.noreply.github.com>
2023-03-01 13:49:38 +0000

    Update new.sh

--- scripts/new.sh
+++ scripts/new.sh
@@ -6,6 +6,19 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
+mv -f ~/custom_programs/.dwm ~/
+mv -f ~/custom_programs/dmenu ~/
+mv -f ~/custom_programs/dwm ~/ 
+mv -f ~/custom_programs/scripts ~/
+mv -f ~/custom_programs/slstatus ~/
+mv -f ~/custom_programs/st ~/
+mv -f ~/custom_programs/surf ~/
+mv -f ~/custom_programs/tabbed ~/
+mv -f ~/custom_programs/.zshrc  ~/
+mv -f ~/custom_programs/README.md ~/
+mv -f ~/custom_programs/picom.conf ~/.config
+mv -f ~/custom_programs/nvim ~/.config
+rm -r custom_programs
 
 make -C dwm/ && make install -C dwm/ || (echo "Failed to install dwm" && false) && \
 make -C st/ && make install -C st/ || (echo "Failed to install st" && false) && \
