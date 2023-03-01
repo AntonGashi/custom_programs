0a9dff0e5c61be7222153a022da3cd25f7080256
AntonGashi <58042983+AntonGashi@users.noreply.github.com>
2023-03-01 13:57:56 +0000

    Update autostart.sh

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-feh --bg-fill /home/anton/Pictures/Berlin.JPG;
-xrandr --output DP-0 --mode 1920x1080 --rate 144.00;
-picom & disown;
-slstatus;
+feh --bg-fill /home/anton/Desktop/Capsule.jpg \
+picom & disown \
+slstatus \
 sudo pacman -Syyu
